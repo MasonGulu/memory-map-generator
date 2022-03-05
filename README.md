e9921350fd361c51271540b4def87abcc7e8a235
Mason <mason.gulu@gmail.com>
2022-03-05 13:29:16 -0500

    Made some UI changes and added a readme. The code is a mess and I suggest, for your sanity, don't look.

---
+++ README.md
@@ -0,0 +1,8 @@
+# This is a tool to generate visual memory map layouts for the IBM 5150 and compatibles.
+Requirements
+* PySimpleGUI
+* Pillow
+* Python 3+
+
+Example output
+![Upper 640k](upper640k.jpg)
--- mapgen_horizontal.py
+++ mapgen_horizontal.py
@@ -1,10 +1,22 @@
 from tkinter import Checkbutton
+from PIL import ImageGrab
 import PySimpleGUI as sg
 import copy
 import math
 import pickle
+import logging
 
+# TODO
+# Change to dark theme
+# Add text color customization for memory "blocks"
+# Change memory "blocks" to classes
+# If I can, make it not ugly
+# Eventually implement if __name__ == "__main__"
+
+sg.theme("DarkBlack")
+logging.basicConfig(level=logging.DEBUG)
 ### Function declaration
+
 
 def hexString(number, minLen=4):
     # Takes in 2 ints: number and minLen. number is the number to be converted to hex
@@ -66,7 +78,8 @@ memoryBlockTemplate = {
     'endSegment': 0x0100,
     'endOffset': 0,
     'label': '',
-    'color': 'red'
+    'color': 'red',
+    'textColor': 'black'
 }
 
 def memoryBlockGUI(memoryBlock=copy.deepcopy(memoryBlockTemplate)):
@@ -76,40 +89,43 @@ def memoryBlockGUI(memoryBlock=copy.deepcopy(memoryBlockTemplate)):
     fullBoxWidth = 11
     dashPaddingWidth = 18
     # End size constants
-    startFrameLayout = [
-        [sg.Text('Segment:',size=(segBoxWidth,1)), sg.Input(hexString(memoryBlock['startSegment']), 
-            size=(segBoxWidth,1), key='startSegment', enable_events=True)],
-        [sg.Text('Offset:',size=(segBoxWidth,1)), sg.Input(hexString(memoryBlock['startOffset']), 
-            size=(segBoxWidth,1), key='startOffset', enable_events=True)],
-        [sg.Text('-'*dashPaddingWidth + ' OR ' + '-'*dashPaddingWidth)],
-        [sg.Text('Full address:'), sg.Input(hexString(calculateFullAddr(memoryBlock['startSegment'], memoryBlock['startOffset']),5), 
-            size=(fullBoxWidth,1), key='startFullAddr', enable_events=True)]
-            # I tried to stay within PEP 8, but this line is just oh god oh fuck
+    startTabsLayout = [
+        sg.Tab('Components', 
+            [[sg.Text('Segment:',size=(segBoxWidth,1)), 
+                sg.Input(hexString(memoryBlock['startSegment']), expand_x=True, key='startSegment', enable_events=True)],
+            [sg.Text('Offset:',size=(segBoxWidth,1)), 
+                sg.Input(hexString(memoryBlock['startOffset']), expand_x=True, key='startOffset', enable_events=True)]]),
+        sg.Tab('Full Address',[
+            [sg.Text('Full address:'), 
+            sg.Input(hexString(calculateFullAddr(memoryBlock['startSegment'], 
+                memoryBlock['startOffset']),5), expand_x=True, key='startFullAddr', enable_events=True)]])
     ]
-    endFrameLayout = [
-        [sg.Text('Segment:',size=(segBoxWidth,1)), sg.Input(hexString(memoryBlock['endSegment']), 
-            size=(segBoxWidth,1), key='endSegment', enable_events=True)],
-        [sg.Text('Offset:',size=(segBoxWidth,1)), sg.Input(hexString(memoryBlock['endOffset']), 
-            size=(segBoxWidth,1), key='endOffset', enable_events=True)],
-        [sg.Text('-'*dashPaddingWidth + ' OR ' + '-'*dashPaddingWidth)],
-        [sg.Text('Full address:'), sg.Input(hexString(calculateFullAddr(memoryBlock['endSegment'], memoryBlock['endOffset']),5), 
-            size=(fullBoxWidth,1), key='endFullAddr', enable_events=True)],
-        [sg.Text('-'*dashPaddingWidth + ' OR ' + '-'*dashPaddingWidth)],
-        [sg.Text('Length:',size=(segBoxWidth,1)), sg.Input(hexString(calculateSize(memoryBlock['startSegment'], 
-            memoryBlock['startOffset'], memoryBlock['endSegment'], memoryBlock['endOffset'])),size=(segBoxWidth,1),key='length', enable_events=True)]
-            # You'd think I was kidding
+    endTabsLayout = [
+        sg.Tab('Components',
+            [[sg.Text('Segment:',size=(segBoxWidth,1)), sg.Input(hexString(memoryBlock['endSegment']), 
+                expand_x=True, key='endSegment', enable_events=True)],
+            [sg.Text('Offset:',size=(segBoxWidth,1)), sg.Input(hexString(memoryBlock['endOffset']), 
+                expand_x=True, key='endOffset', enable_events=True)]]),
+        sg.Tab('Full Address',
+            [[sg.Text('Full address:'), sg.Input(hexString(calculateFullAddr(memoryBlock['endSegment'], memoryBlock['endOffset']),5), 
+                expand_x=True, key='endFullAddr', enable_events=True)]]),
+        sg.Tab('Length',
+            [[sg.Text('Length:'), sg.Input(hexString(calculateSize(memoryBlock['startSegment'],
+                memoryBlock['startOffset'], memoryBlock['endSegment'], memoryBlock['endOffset'])),expand_x=True,size=(segBoxWidth,1),key='length', enable_events=True)]])
     ]
     infoFrameLayout = [
-        [sg.Text('Label:'), sg.Input(memoryBlock['label'],size=(fullBoxWidth,1),key='label', enable_events=True)],
-        [sg.Text('Color:'), sg.Input(memoryBlock['color'],size=(fullBoxWidth,1),key='color', enable_events=True),
-            sg.ColorChooserButton(' ',button_color=(None, memoryBlock['color']),key='chooser')]
+        [sg.Text('Label: '), sg.Input(memoryBlock['label'],size=(fullBoxWidth,1),key='label', enable_events=True, expand_x=True)],
+        [sg.Text('Color: '), sg.Input(memoryBlock['color'],size=(fullBoxWidth,1),key='color', enable_events=True, expand_x=True),
+            sg.ColorChooserButton(' ',button_color=(None, memoryBlock['color']),key='chooser')],
+        [sg.Text('TColor:'), sg.Input(memoryBlock['textColor'],size=(fullBoxWidth,1),key='textColor', enable_events=True, expand_x=True),
+            sg.ColorChooserButton(' ',button_color=(None, memoryBlock['color']), key="textColorChooser")]
     ]
 
     layout = [
         [sg.Text('Create or Edit a Memory Block')],
-        [sg.Frame('Info', infoFrameLayout)],
-        [sg.Frame('Start', startFrameLayout)],
-        [sg.Frame('End', endFrameLayout)],
+        [sg.Frame('Info', infoFrameLayout, expand_x=True)],
+        [sg.Frame('Start', [[sg.TabGroup([startTabsLayout])]]),
+            sg.Frame('End', [[sg.TabGroup([endTabsLayout])]])],
         [sg.Cancel(key='cancel'),sg.Submit(key='submit')]
     ]
     win = sg.Window('Memory Block GUI', layout)
@@ -118,7 +134,7 @@ def memoryBlockGUI(memoryBlock=copy.deepcopy(memoryBlockTemplate)):
 
         if (event == sg.WIN_CLOSED) or (event == 'cancel'):
             win.close()
-            return backup
+            return False
         
         if (event == 'submit'):
             win.close()
@@ -161,9 +177,12 @@ def memoryBlockGUI(memoryBlock=copy.deepcopy(memoryBlockTemplate)):
                 win['endOffset'].update(value=hexString(memoryBlock['endOffset']))
             
             if (event == 'color'):
-                win['color'].update(value=values['color'])
                 win['chooser'].update(button_color=(None, values['color']))
                 memoryBlock['color'] = values['color']
+
+            if (event == 'textColor'):
+                win['textColorChooser'].update(button_color=(None, values['textColor']))
+                memoryBlock['textColor'] = values['textColor']
 
             if (event == 'label'):
                 memoryBlock['label'] = values['label']
@@ -173,8 +192,8 @@ def memoryBlockGUI(memoryBlock=copy.deepcopy(memoryBlockTemplate)):
 
 
             win.finalize() 
-        except:
-            print("Exception occured!")
+        except ValueError:
+            logging.info("Value in a input is not a number.")
 
 def isAnythingClose(xpos, posTable):
     threshold = 8
@@ -183,11 +202,24 @@ def isAnythingClose(xpos, posTable):
             return True
     return False
 
+# Taken from https://github.com/PySimpleGUI/PySimpleGUI/blob/master/DemoPrograms/Demo_Graph_Drawing_And_Dragging_Figures.py
+def save_element_as_file(element, filename):
+    """
+    Saves any element as an image file.  Element needs to have an underlyiong Widget available (almost if not all of them do)
+    :param element: The element to save
+    :param filename: The filename to save to. The extension of the filename determines the format (jpg, png, gif, ?)
+    """
+    widget = element.Widget
+    box = (widget.winfo_rootx(), widget.winfo_rooty(), widget.winfo_rootx() + widget.winfo_width(), widget.winfo_rooty() + widget.winfo_height())
+    grab = ImageGrab.grab(bbox=box)
+    grab.save(filename)
+
 mapInfo = {
     'memoryBlocks': [],
     'startAddress': 0,
     'endAddress': 0xFFFFF,
     'color': 'white',
+    'textColor': 'white',
     'width': 500,
     'height': 120
 }
@@ -211,17 +243,19 @@ mapInfo = {
 #]
 
 layout = [
-    [sg.Text('Starting Address:'), sg.Input('0x00000',key='startAddress')],
-    [sg.Text('Ending Address:'), sg.Input('0xFFFFF',key='endAddress')],
-    [sg.Text('Color:'), sg.Input('#FFFFFF',key='color'), sg.ColorChooserButton(' ',button_color=(None, mapInfo['color']),key='chooser')],
-        # Like I said, fuck this
-    [sg.Text('Width:'), sg.Input('500', key='width')],
-    [sg.Text('Height:'), sg.Input('100', key='height')],
+    [sg.Text('Starting Address:'), sg.Input('0x00000',key='startAddress',expand_x=True)],
+    [sg.Text('Ending Address:'), sg.Input('0xFFFFF',key='endAddress',expand_x=True)],
+    [sg.Text('Color:'), sg.Input('#FFFFFF',key='color',expand_x=True), sg.ColorChooserButton(' ',button_color=(None, mapInfo['color']),key='chooser')],
+    [sg.Text('Text Color:'), sg.Input('#FFFFFF',key='textColor',expand_x=True), sg.ColorChooserButton(' ',button_color=(None, mapInfo['textColor']))],
+    [sg.Text('Width:'), sg.Input('500', key='width',expand_x=True)],
+    [sg.Text('Height:'), sg.Input('100', key='height',expand_x=True)],
     [sg.Submit()]
 ]
 
 win = sg.Window('Memory Map Setup', layout)
 event, values = win.read()
+if event != "Submit":
+    exit()
 mapInfo['startAddress'] = int(values['startAddress'],0)
 mapInfo['endAddress'] = int(values['endAddress'], 0)
 
@@ -243,15 +277,17 @@ win.close()
 
 layout = [
     [sg.Graph((mapInfo['fWidth'], mapInfo['fHeight']), (0,mapInfo['fHeight']), (mapInfo['fWidth'],0),key='graph')],
-    [sg.Text('Zoom',size=(8,1)), sg.Slider(range=(1,100), key='divisor',orientation='horizontal',enable_events=True, size=(60,10),disable_number_display=True)],
-    [sg.Text('Location',size=(8,1)), sg.Slider(range=(0,0), key='location',orientation='horizontal',enable_events=True,disable_number_display=True,
-        size=(60,10))],
-    [sg.Listbox(mapInfo['memoryBlocks'], size=(25,4), key='selectedBlock'), sg.VerticalSeparator(),
+    [sg.Text('Zoom',size=(8,1)), 
+        sg.Slider(range=(1,100), key='divisor',orientation='horizontal',enable_events=True, expand_x=True,disable_number_display=True)],
+    [sg.Text('Location',size=(8,1)), 
+        sg.Slider(range=(0,0), key='location',orientation='horizontal',enable_events=True,disable_number_display=True,expand_x=True)],
+    [sg.Listbox(mapInfo['memoryBlocks'], size=(25,4), key='selectedBlock',expand_x=True), sg.VerticalSeparator(),
         sg.Button('New',key='new'), sg.Button('Edit',key='edit'), sg.Button('Delete',key='delete'),
         sg.Checkbox('Fit Camera to Segment', key='segCam', enable_events=True)],
     [sg.Checkbox('Enable Segment', key='segEnable', enable_events=True), sg.Input('0', size=(8,1),key='segment',enable_events=True),
-    sg.Slider(range=(0,0xFFF0), orientation='h', size=(45,10), key='segmentSlider', enable_events=True,disable_number_display=True,resolution=0x0010)],
-    [sg.Input(key='file'), sg.FileSaveAs('Browse'), sg.Button('Save', key='save'), sg.Button('Load',key='load')],
+        sg.Slider(range=(0,0xFFF0), orientation='h', key='segmentSlider', enable_events=True,
+            expand_x=True, disable_number_display=True,resolution=0x0010)],
+    [sg.Input(key='file'), sg.FileSaveAs('Browse'), sg.Button('Save', key='save'), sg.Button('Load',key='load'), sg.Button('SaveJPG',key='saveJPG')],
     
     
 ]
@@ -274,7 +310,7 @@ while True:
     try:
         win['segment'].update(value=hexString(int(values['segment'],0),4))
     except:
-        print('Value in segment box is invalid!')
+        logging.info("Data in segment box is invalid.")
 
     mapInfo['startAddress'] = mapInfoBackup['startAddress']
     mapInfo['endAddress'] = mapInfoBackup['endAddress']
@@ -286,18 +322,23 @@ while True:
                 pickle.dump(mapInfo, file)
                 file.close()
         except:
-            print('filename probably invalid')
+            logging.warning("Invalid filename.")
 
-    if (event == 'load'):
+    elif (event == 'load'):
         try:
+            tmpwidth = mapInfo['width']
+            tmpheight = mapInfo['height']
             with open(values['file'], 'rb') as file:
                 mapInfo = pickle.load(file)
                 mapInfoBackup = copy.deepcopy(mapInfo)
                 file.close()
-        except:
-            print('filename probably invalid')
+            mapInfo['width'] = tmpwidth
+            mapInfo['height'] = tmpheight
+            win['segmentSlider'].update(range=(mapInfo['startAddress']>>4, mapInfo['endAddress']>>4))
+        except Exception as a:
+            logging.warning("Invalid filename. %s",a)
 
-    if (event == 'segmentSlider'):
+    elif (event == 'segmentSlider'):
         try:
             win['segment'].update(value = hexString(int(values['segmentSlider']), 4))
             values['segment'] = hexString(int(values['segmentSlider']), 4)
@@ -318,19 +359,26 @@ while True:
 
 
     if (event == 'new'):
-        mapInfo['memoryBlocks'].append(copy.deepcopy(memoryBlockGUI()))
+        a = memoryBlockGUI()
+        if a:
+            mapInfo['memoryBlocks'].append(copy.deepcopy(a))
 
-    if (event == 'delete'):
-        try:
-            del mapInfo['memoryBlocks'][labels.index(values['selectedBlock'][0])]
-        except:
-            print('Error in deletion')
+    elif (event == 'delete'):
+        del mapInfo['memoryBlocks'][labels.index(values['selectedBlock'][0])]
     
-    if (event == 'edit'):
+    elif (event == 'edit'):
         try:
-            mapInfo['memoryBlocks'][labels.index(values['selectedBlock'][0])] = copy.deepcopy(memoryBlockGUI(mapInfo['memoryBlocks'][labels.index(values['selectedBlock'][0])]))
+            a = memoryBlockGUI(mapInfo['memoryBlocks'][labels.index(values['selectedBlock'][0])])
+            if a:
+                mapInfo['memoryBlocks'][labels.index(values['selectedBlock'][0])] = copy.deepcopy(a)
         except:
             print('Error in editing')
+
+    elif (event == 'saveJPG'):
+        try:
+            save_element_as_file(win['graph'], values['file'])
+        except ValueError:
+            logging.info("Invalid filename.")
 
     # Reorganize memory blocks based off starting address
     notSorted = True
@@ -348,23 +396,23 @@ while True:
     win['graph'].draw_rectangle((mapInfo['left'],mapInfo['top']),
         (mapInfo['left']+mapInfo['width'],mapInfo['top']+mapInfo['height']),
         fill_color=mapInfo['color'])
-    win['graph'].draw_text(hexString(mapInfo['startAddress'],5), (mapInfo['left']-10,mapInfo['top']+(mapInfo['height']/2)),angle=90)
-    win['graph'].draw_text(hexString(mapInfo['endAddress'],5), (mapInfo['left']+mapInfo['width']+10,mapInfo['top']+(mapInfo['height']/2)),angle=90)
+    win['graph'].draw_text(hexString(mapInfo['startAddress'],5), (mapInfo['left']-10,mapInfo['top']+(mapInfo['height']/2)),angle=90, color=mapInfo['textColor'])
+    win['graph'].draw_text(hexString(mapInfo['endAddress'],5), (mapInfo['left']+mapInfo['width']+10,mapInfo['top']+(mapInfo['height']/2)),angle=90, color=mapInfo['textColor'])
 
     xposText = []
     labels = []
     for x in mapInfo['memoryBlocks']:
         labels.append(x['label'])
         win['graph'].draw_rectangle(topLeft(x), bottomRight(x), fill_color=x['color'])
-        win['graph'].draw_text(x['label'], center(x), angle=90)
+        win['graph'].draw_text(x['label'], center(x), color = x['textColor'], angle=90)
         if not isAnythingClose(topLeft(x)[0]+values['horOff'], xposText):
             win['graph'].draw_text(hexString(calculateFullStartAddr(x),5), topLeft(x, values['horOff'], -values['verOff']), 
-                text_location=sg.TEXT_LOCATION_LEFT, angle=values['angle'])
+                text_location=sg.TEXT_LOCATION_LEFT, angle=values['angle'], color=mapInfo['textColor'])
             xposText.append(topLeft(x)[0]+values['horOff'])
 
         if not isAnythingClose(topRight(x)[0]-values['horOff'], xposText):
             win['graph'].draw_text(hexString(calculateFullEndAddr(x),5), topRight(x, -values['horOff'], -values['verOff']), 
-                text_location=sg.TEXT_LOCATION_LEFT, angle=values['angle'])
+                text_location=sg.TEXT_LOCATION_LEFT, angle=values['angle'], color=mapInfo['textColor'])
             xposText.append(topRight(x)[0]-values['horOff'])
         
         if values['segEnable']:
@@ -373,7 +421,7 @@ while True:
                 if (calculateFullStartAddr(x) - (int(values['segment'],0)<<4) >= 0 and calculateFullStartAddr(x) - (int(values['segment'],0)<<4) <= 0xffff):
                     # Valid to show
                     win['graph'].draw_text(hexString(calculateFullStartAddr(x)-(int(values['segment'],0)<<4),4), bottomLeft(x, 0, values['verOff']),
-                        text_location=sg.TEXT_LOCATION_RIGHT, angle=values['angle'])
+                        text_location=sg.TEXT_LOCATION_RIGHT, angle=values['angle'], color=mapInfo['textColor'])
             except:
                 pass
                 # Fail silently as we're already printing a message about this
@@ -381,15 +429,15 @@ while True:
         try:
             win['graph'].draw_line((mapInfo['left']+bytesToPixel((int(values['segment'],0)<<4)-mapInfo['startAddress']), mapInfo['top']),
             (mapInfo['left']+bytesToPixel((int(values['segment'],0)<<4)-mapInfo['startAddress']), mapInfo['top']+mapInfo['height']),
-            color='blue', width=2)
+            color='grey', width=2)
 
             win['graph'].draw_line((mapInfo['left']+bytesToPixel((int(values['segment'],0)<<4)-mapInfo['startAddress']), mapInfo['top']+mapInfo['height']),
             (mapInfo['left']+bytesToPixel((int(values['segment'],0)<<4)-mapInfo['startAddress']+0xFFFF), mapInfo['top']+mapInfo['height']),
-            color='blue', width=2)
+            color='grey', width=2)
 
             win['graph'].draw_line((mapInfo['left']+bytesToPixel((int(values['segment'],0)<<4)-mapInfo['startAddress']+0xFFFF), mapInfo['top']+mapInfo['height']),
             (mapInfo['left']+bytesToPixel((int(values['segment'],0)<<4)-mapInfo['startAddress']+0xFFFF), mapInfo['top']),
-            color='blue', width=2)
+            color='grey', width=2)
         except:
             pass
     
